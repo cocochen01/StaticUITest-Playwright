3d4dc47cbf89bd523e6b15c1d131b0230e5b255f
Coco <cocochen12301@gmail.com>
2025-08-29 03:22:39 -0700

    feat: Create past page fixture

--- fixtures/custom-fixtures.ts
+++ fixtures/custom-fixtures.ts
@@ -1,12 +1,13 @@
 import { test as base, Page } from '@playwright/test';
 import { NewestPages } from "../page-objects/newest-pages";
 import { PageObject } from "../page-objects/page-object";
-import { getNewestPageFile, HOMEPAGE_FILE, NEWESTPAGE_COUNT, SAVED_PAGES_FOLDER } from '../global-values';
+import { getNewestPageFile, HOMEPAGE_FILE, NEWESTPAGE_COUNT, PASTPAGE_FILE, SAVED_PAGES_FOLDER } from '../global-values';
 import fs from 'fs';
 
-type Fixtures = {
+interface Fixtures {
   homepage: PageObject;
   newestPages: NewestPages;
+  pastPage: PageObject;
 };
 
 export const test = base.extend<Fixtures>({
@@ -29,5 +30,12 @@ export const test = base.extend<Fixtures>({
     const newestPages = new NewestPages(pageArray);
     await newestPages.setPagesContent(contentArray);
     await use(newestPages);
+  },
+  pastPage: async ({ page }, use) => {
+    const pastPageContent: string = fs.readFileSync(SAVED_PAGES_FOLDER + "/" + PASTPAGE_FILE, "utf-8");
+
+    const pastPage = new PageObject(page);
+    await pastPage.setPageContent(pastPageContent);
+    await use(pastPage);
   },
 });
